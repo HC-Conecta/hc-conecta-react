73ec45a6894a34b29e762790cd23d7e9a494a948
enricodelesporte <enricodelesporte@hotmail.com>
2025-08-22 20:38:04 -0300

    feat: update NotFound page with new layout and navigation button

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,12 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import H1 from "@/components/H1";
+import Button from "@/components/Button";
 
 const NotFound = () => {
   const location = useLocation();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.error(
@@ -13,12 +17,17 @@ const NotFound = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
-        </a>
+      <div className="text-center flex flex-col justify-center items-center">
+        <H1>
+          404
+        </H1>
+        <div className="w-64 h-auto">
+          <img src="/dinosaur-notfound.png" alt="Dinosaurs" />
+        </div>
+        <p className="text-xl text-gray-600 mb-4 max-w-[25rem]">Oops! Nosso dinossaurinho médico também não conseguiu achar esse caminho.</p>
+        <Button className="text-white" variant="primary" onClick={() => navigate("/home")}>
+          Voltar para a Home
+        </Button>
       </div>
     </div>
   );
