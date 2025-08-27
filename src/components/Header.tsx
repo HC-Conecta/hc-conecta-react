10ba8ac982ec84b5f26f954f3ac5f1e715e5e7e3
iagoliziero <iagolizierox14@gmail.com>
2025-08-27 12:40:35 -0300

    feat: update mobile breakpoint to improve responsiveness in Header and navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { NavigationItem } from '../types/global';
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { NavigationItem } from "../types/global";
+import Button from "./Button";
+import { LogIn, UserRoundPlus, UserRoundPlusIcon } from "lucide-react";
+import { se } from "date-fns/locale";
 
 const Header: React.FC = () => {
   const location = useLocation();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const navigationItems: NavigationItem[] = [
-    { name: 'Início', path: '/home' },
-    { name: 'Como Acessar', path: '/guide' },
-    { name: 'Agendar Consulta', path: '/appointment' },
-    { name: 'Dúvidas', path: '/doubt' },
-    { name: 'Suporte', path: '/support' },
-    { name: 'Sobre Nós', path: '/about' }
+    { name: "Início", path: "/home" },
+    { name: "Como Acessar", path: "/guide" },
+    { name: "Agendar Consulta", path: "/appointment" },
+    { name: "Dúvidas", path: "/doubt" },
+    { name: "Suporte", path: "/support" },
+    { name: "Sobre Nós", path: "/about" },
+    { name: "Entrar", path: "/login" },
+    { name: "Criar Conta", path: "/register" },
   ];
 
   const navigate = useNavigate();
@@ -24,57 +29,130 @@ const Header: React.FC = () => {
           {/* Logo */}
           <div className="flex items-center space-x-3">
             <div className="w-full p-4 bg-gradient-primary rounded-lg flex items-center justify-center">
-              <img onClick={()=> navigate('/home')} className='h-14 cursor-pointer' src="/logo-hc-conecta.png" alt="Logo HC Conecta" />
+              <img
+                onClick={() => navigate("/home")}
+                className="h-14 cursor-pointer"
+                src="/logo-hc-conecta.png"
+                alt="Logo HC Conecta"
+              />
             </div>
-        
           </div>
 
           {/* Desktop Navigation */}
-          <nav className="hidden md:flex space-x-1">
-            {navigationItems.map((item) => (
-              <Link
-                key={item.path}
-                to={item.path}
-                className={`px-4 py-2 rounded-lg text-md font-medium transition-colors ${
-                  location.pathname === item.path
-                    ? 'bg-blue-700 text-white'
-                    : 'text-foreground hover:bg-accent hover:text-accent-foreground'
-                }`}
-              >
-                {item.name}
-              </Link>
-            ))}
+          <nav className="hidden xl:flex space-x-1">
+          {navigationItems.map((item) =>
+                item.name !== "Entrar" && item.name !== "Criar Conta" && (
+                  <button
+                    key={item.path}
+                    onClick={() => {
+                      setIsMobileMenuOpen(false);
+                      navigate(item.path);
+                    }}
+                    className={`block px-4 py-3 rounded-lg text-base font-medium transition-colors ${
+                      location.pathname === item.path
+                        ? "bg-blue-700 text-white"
+                        : "text-foreground hover:bg-accent hover:text-accent-foreground"
+                    }`}
+                  >
+                    {item.name}
+                  </button>
+                ) 
+              )}
           </nav>
-
+          <div className="flex gap-3">
+            <Button
+              onClick={() => navigate("/login")}
+              size="sm"
+              className="hidden xl:flex gap-2 items-center justify-center "
+              variant="outline"
+            >
+              {" "}
+              <i>
+                <LogIn size={18} />{" "}
+              </i>{" "}
+              Entrar{" "}
+            </Button>
+            <Button
+              onClick={() => navigate("/register")}
+              size="sm"
+              className="hidden xl:flex gap-2 items-center text-white justify-center"
+              variant="primary"
+            >
+              {" "}
+              <i>
+                <UserRoundPlus size={18} className="text-white" />{" "}
+              </i>{" "}
+              Criar Conta{" "}
+            </Button>
+          </div>
           {/* Mobile menu button */}
           <button
             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-            className="md:hidden p-2 rounded-lg text-foreground hover:bg-accent"
+            className="xl:hidden p-2 rounded-lg text-foreground hover:bg-accent"
             aria-label="Abrir menu"
           >
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
             </svg>
           </button>
         </div>
 
         {/* Mobile Navigation */}
         {isMobileMenuOpen && (
-          <div className="md:hidden py-4 border-t border-border">
+          <div className="xl:hidden py-4 border-t border-border ">
             <nav className="space-y-2">
-              {navigationItems.map((item) => (
-                <Link
-                  key={item.path}
-                  to={item.path}
-                  onClick={() => setIsMobileMenuOpen(false)}
-                  className={`block px-4 py-3 rounded-lg text-base font-medium transition-colors ${
-                    location.pathname === item.path
-                      ? 'bg-blue-700 text-white'
-                      : 'text-foreground hover:bg-accent hover:text-accent-foreground'
-                  }`}
-                >
-                  {item.name}
-                </Link>
+              {navigationItems.map((item) =>
+                item.name !== "Entrar" && item.name !== "Criar Conta" ? (
+                  <button
+                    key={item.path}
+                    onClick={() => {
+                      setIsMobileMenuOpen(false);
+                      navigate(item.path);
+                    }}
+                    className={`block px-4 py-3 rounded-lg text-base font-medium transition-colors ${
+                      location.pathname === item.path
+                        ? "bg-blue-700 text-white"
+                        : "text-foreground hover:bg-accent hover:text-accent-foreground"
+                    }`}
+                  >
+                    {item.name}
+                  </button>
+                ) : item.name === "Entrar" ? (
+                  <div className="flex items-center px-2 py-3 gap-2">
+                    <i> <LogIn size={18} className="text-blue-700" /></i>
+                    <button
+                    key={item.path}
+                    onClick={() => {
+                      setIsMobileMenuOpen(false);
+                      navigate(item.path);
+                    }}
+                    className="block rounded-lg font-medium transition-colors text-blue-700"
+                  > {item.name} </button>
+                  
+                  </div>
+              ) : (
+                <div className="flex items-center px-2 py-3 gap-2">
+                    <i> <UserRoundPlusIcon size={18} className="text-blue-700" /></i>
+                    <button
+                    key={item.path}
+                    onClick={() => {
+                      setIsMobileMenuOpen(false);
+                      navigate(item.path);
+                    }}
+                    className="block rounded-lg font-medium transition-colors text-blue-700 "
+                  > {item.name} </button>
+                  
+                  </div>
               ))}
             </nav>
           </div>
