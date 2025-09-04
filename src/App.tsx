dccca203c9eadadf33154a67eb4a660995ee95b4
iagoliziero <iagolizierox14@gmail.com>
2025-09-04 10:53:30 -0300

    feat: remove unused Layout component and clean up App component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./components/AppRoutes";
 
 const queryClient = new QueryClient();
