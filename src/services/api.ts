d9c5ffa229c370e8e0916d3b6ab06a2636abf22e
enricodelesporte <enricodelesporte@hotmail.com>
2025-10-21 11:01:32 -0300

    feat: implement GET endpoint (verifyUser) in Services Path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import { NameValues } from "@/interfaces/global";
+import { IprofileData, NameValues } from "@/interfaces/global";
 
 export const createUser = async (data: NameValues) => {
 
@@ -30,3 +30,24 @@ export const createUser = async (data: NameValues) => {
         console.log(`Error create User: ${error}`);
     }
 }
+
+export const verifyUser = async (data: NameValues ) => {
+    
+    data.cpf = data.cpf.replace(/\D/g, "");
+
+    const BASE_URL: string = `${import.meta.env.VITE_API_URL}?cpf=${data.cpf}&password=${data.password}`;
+
+    try {
+        const response = await fetch(BASE_URL, { method: "GET" });
+        const data = await response.json();
+        if (data.length > 0) {
+            const user: IprofileData = data[0];
+            localStorage.setItem("userId", user.id);
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
