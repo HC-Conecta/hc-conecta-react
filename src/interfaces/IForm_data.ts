bdfe0c23f2d69445f0324a96dd99e396005b0570
enricodelesporte <enricodelesporte@hotmail.com>
2025-10-22 08:44:42 -0300

    feat: refactoring Interface Form_data

---
+++ src/interfaces/IForm_data.ts
@@ -0,0 +1,10 @@
+interface IForm_data {
+    name: string;
+    email: string;
+    telephone: string;
+    message: string;
+    needsHelp: boolean;
+    serviceType: string;
+}
+
+export default IForm_data;
