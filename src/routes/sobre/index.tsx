75f918b0125c014f935bdd6b1d6ca80c5d4d3bed
vitordias1006 <vitordias09532@gmail.com>
2025-11-03 10:02:16 -0300

    refactor: refactoring Sobre page for better structure

--- src/routes/sobre/index.tsx
+++ src/routes/sobre/index.tsx
@@ -4,9 +4,10 @@ import { CircleCheck, CircleUser, Heart, Lightbulb, Mail } from "lucide-react";
 import { Paragraph } from "@/components/ui/textos/Paragraph";
 import TextToSpeechButton from "@/components/tts/TextToSpeechButton";
 import { developers } from "@/data/developer";
+import { H3 } from "@/components/ui/textos/H3";
+import H2 from "@/components/ui/textos/H2";
 
 const About: React.FC = () => {
-
   return (
     <div className="bg-background py-16">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -28,16 +29,14 @@ const About: React.FC = () => {
 
         {/* Mission */}
         <div className="rounded-xl p-8 shadow-md border border-border mb-16">
-          <h2 className="text-2xl font-bold text-foreground mb-4">
-            Nossa Missão
-          </h2>
-          <p className="text-lg text-muted-foreground leading-relaxed">
+          <H3 isStronger={true}>Nossa Missão</H3>
+          <Paragraph>
             Criar um ambiente digital inclusivo e acessível que permita a todas
             as pessoas, independentemente de sua familiaridade com a tecnologia,
             acessarem facilmente os serviços do Hospital das Clínicas.
             Acreditamos que o acesso à saúde deve ser simples, claro e livre de
             barreiras tecnológicas.
-          </p>
+          </Paragraph>
           <div className="mt-5 flex items-start ">
             <TextToSpeechButton
               colorIsBlue
@@ -48,36 +47,32 @@ const About: React.FC = () => {
 
         {/* Team */}
         <div className="mb-16">
-          <h2 className="text-3xl font-bold text-foreground text-center mb-12">
-            Conheça Nossa Equipe
-          </h2>
-          <div className="grid md:grid-cols-3 gap-8">
+          <H2>Conheça Nossa Equipe</H2>
+          <div className="grid md:grid-cols-3 gap-8 mt-5">
             {developers.map((developer, index) => (
               <div
                 key={index}
-                className="bg-surface rounded-xl items-center justify-center p-6 shadow-md border border-border text-center hover:shadow-lg transition-shadow"
+                className="bg-surface rounded-xl p-6 shadow-md border border-border text-center hover:shadow-lg transition-shadow flex flex-col items-center"
               >
-                <div className="bg-gradient-primary rounded-full mx-auto mb-4 flex items-center justify-center">
+                <div className="bg-gradient-primary rounded-full mb-4 flex items-center justify-center w-48 h-48">
                   <div className="w-44 h-44 rounded-full overflow-hidden border-4 border-white">
-                    <img src={developer.image} alt={developer.name} />
+                    <img
+                      src={developer.image}
+                      alt={developer.name}
+                      className="object-cover w-full h-full"
+                    />
                   </div>
                 </div>
-                <h3 className="text-xl text-blue-700 font-semibold text-foreground mb-2">
+
+                <h3 className="text-xl font-semibold text-blue-700 mb-2">
                   {developer.name}
                 </h3>
-                <p className="text-lg font-medium mb-3">{developer.role}</p>
-                <p className="text-muted-foreground text-md">
-                  {developer.description}
+
+                <Paragraph>{developer.description}</Paragraph>
+
+                <p className="text-lg font-medium mt-4">
+                  Turma: <span className="font-semibold">1TDSPG</span>
                 </p>
-                <div className="flex items-start mt-4">
-                 <p className="text-lg font-medium mb-3">Turma: 1TDSPG </p>
-                </div>
-                <div className="mt-5 flex items-start ">
-                  <TextToSpeechButton
-                    colorIsBlue
-                    text={`${developer.name}. ${developer.role}. ${developer.description}`}
-                  />
-                </div>
               </div>
             ))}
           </div>
@@ -85,18 +80,14 @@ const About: React.FC = () => {
 
         {/* Values */}
         <div className="bg-accent rounded-xl p-8 mb-16">
-          <h2 className="text-2xl font-bold text-foreground mb-6 text-center">
-            Nossos Valores
-          </h2>
-          <div className="grid md:grid-cols-2 gap-8">
+          <H2>Nossos Valores</H2>
+          <div className="grid md:grid-cols-2 gap-8 mt-5">
             <div className="flex items-start space-x-3">
               <div className="w-6 h-6 text-primary mt-1">
                 <CircleCheck className="text-blue-700" />
               </div>
               <div>
-                <h3 className="font-semibold text-foreground mb-1 text-lg">
-                  Acessibilidade
-                </h3>
+                <H3 isStronger={true}>Acessibilidade</H3>
                 <Paragraph>
                   Criamos soluções que todos podem usar, independente da idade
                   ou habilidade tecnológica.
@@ -115,9 +106,7 @@ const About: React.FC = () => {
                 <Heart className="text-blue-700" />
               </div>
               <div>
-                <h3 className="font-semibold text-foreground mb-1 text-lg">
-                  Empatia
-                </h3>
+                <H3 isStronger={true}>Empatia</H3>
                 <Paragraph>
                   Entendemos as dificuldades e criamos soluções pensando no
                   usuário em primeiro lugar.
@@ -137,9 +126,7 @@ const About: React.FC = () => {
                 <Lightbulb className="text-blue-700" />
               </div>
               <div>
-                <h3 className="font-semibold text-foreground mb-1 text-lg">
-                  Simplicidade
-                </h3>
+                <H3 isStronger={true}>Simplicidade</H3>
                 <Paragraph>
                   Mantemos tudo simples e direto, sem complicações
                   desnecessárias.
@@ -158,9 +145,7 @@ const About: React.FC = () => {
                 <CircleUser className="text-blue-700" />
               </div>
               <div>
-                <h3 className="font-semibold text-foreground mb-1 text-lg">
-                  Suporte
-                </h3>
+                <H3 isStronger={true}>Suporte</H3>
                 <Paragraph>
                   Oferecemos ajuda personalizada sempre que necessário.
                 </Paragraph>
@@ -188,10 +173,12 @@ const About: React.FC = () => {
             href="mailto:hcconecta@gmail.com"
             className="inline-flex items-center space-x-2 text-primary hover:text-primary/80 transition-colors"
           >
-            <Mail className="w-5 h-5 text-blue-700" />
-            <span className="text-lg text-blue-700 font-medium">
-              hcconecta@gmail.com
-            </span>
+            <div className="flex mt-3 gap-2 items-center">
+              <Mail className="w-5 h-5 text-blue-700" />
+              <span className="text-lg text-blue-700 font-medium">
+                hcconecta@gmail.com
+              </span>
+            </div>
           </a>
           <div className="mt-5 flex items-start justify-center ">
             <TextToSpeechButton
